4e1839cd7138c296c1e4a61531ff45657cd460c5
Bipul Swain <bipulswain365@gmail.com>
2023-05-10 02:55:50 +0530

    Modification of Card Done

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import Home from './Home'
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
+import MiddlePage from './components/MiddlePage';
 function App() {
   return (
 <Router>
   <Routes>
+    <Route path='/middlepage' element={<MiddlePage/>}/>
     <Route path='/login' element={<SignIn/>}/>
     <Route path='/register' element={<SignUp/>}/>
     
