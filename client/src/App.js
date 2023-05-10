47f136147eaa97271c2bfbc24e5d1fd94eb49da5
Bipul Swain <bipulswain365@gmail.com>
2023-05-10 15:15:53 +0530

    sfsf

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import Home from './components/Home/Home';
 import Digo from './components/Dignosis/Digo';
-import Digo from './components/Dignosis/Digo';
 import SingleConnection from './components/Connection/SingleConnection';
 import Connections from './components/Connection/Connections';
 import AllRequests from './components/Connection/AllRequests';
@@ -33,10 +32,26 @@ function App() {
 <NavBar/>
   <Routes>
    
-    {/* <Route path='/middlepage' element={<MiddlePage/>}/> */}
-    <Route path='/login' element={<SignIn/>}/>
-    <Route path='/dignosis' element={<Digo/>}/>
-    <Route path='/register' element={<SignUp/>}/>
+    <Route path='/middlepage' element={
+      <SemiprotectedRoute>
+    <MiddlePage/>
+    </SemiprotectedRoute>
+    }/>
+    <Route path='/login' element={<GuestRoute>
+    <SignIn/>
+    </GuestRoute>
+  }
+    />
+    <Route path='/dignosis' element={
+      <SemiprotectedRoute>
+    <Digo/>
+    </SemiprotectedRoute>
+    }/>
+    <Route path='/register' element={
+    <GuestRoute>
+    <SignUp/>
+    </GuestRoute>
+    }/>
     <Route path='/' element={<Home/>}/>
     <Route path='/connections' element={
       <ProtectedRoute>
