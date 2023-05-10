9022fd10fb6571233212b4a48a09e2b71c8ee7f0
Bipul Swain <bipulswain365@gmail.com>
2023-05-10 11:36:28 +0530

    diagonasis

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import Home from './components/Home/Home';
+import Digo from './components/Digo';
 import SingleConnection from './components/Connection/SingleConnection';
 import Connections from './components/Connection/Connections';
 import AllRequests from './components/Connection/AllRequests';
@@ -14,6 +15,7 @@ function App() {
   <Routes>
     {/* <Route path='/middlepage' element={<MiddlePage/>}/> */}
     <Route path='/login' element={<SignIn/>}/>
+    <Route path='/dignosis' element={<Digo/>}/>
     <Route path='/register' element={<SignUp/>}/>
     <Route path='/' element={<Home/>}/>
     <Route path='/connections' element={<Connections/>}/>
